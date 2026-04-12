3875d5cd03889b2b878c1d2b69714087f6d06e0d
shereen-shaaban <sherinshaban67@gmail.com>
2026-04-12 11:32:15 +0200

    fix : require fields, update relations
    Simplified property configs, enforced required fields, and clarified entity relationships in SalesSystem.Configuration. Updated delete behaviors, removed redundant settings, and improved data integrity across entities.

--- SalesSystem/Configuration/Configurations.cs
+++ SalesSystem/Configuration/Configurations.cs
@@ -17,13 +17,12 @@ namespace SalesSystem.Configuration
 
                 //property configurations
                 builder.HasKey(p => p.Code);
-                builder.Property(p => p.Code).HasColumnType("int").HasPrecision(10, 0);
-                builder.Property(p => p.ProductLineID).HasColumnType("int").HasPrecision(10, 0).IsRequired();
+                builder.Property(p => p.ProductLineID).HasColumnType("int").IsRequired();
                 builder.Property(p => p.Name).HasMaxLength(100).IsRequired();
-                builder.Property(p => p.Scale).IsRequired().HasColumnType("int").HasPrecision(10,0);
+                builder.Property(p => p.Scale).IsRequired();
                 builder.Property(p => p.Vendor).HasMaxLength(100).IsRequired();
                 builder.Property(p => p.ProductDescription).HasMaxLength(500).IsRequired(false);
-                builder.Property(p => p.QuantityINstock).IsRequired().HasColumnType("int").HasPrecision(10, 0);
+                builder.Property(p => p.QuantityINstock).IsRequired();
 				builder.Property(p => p.BuyPrice).HasColumnType("decimal(19,0)").IsRequired();
                 builder.Property(p => p.MSPR).HasMaxLength(20).IsRequired(false);
 
@@ -31,12 +30,12 @@ namespace SalesSystem.Configuration
 				builder.HasIndex(p => p.Name).IsUnique();
                 builder.HasIndex(p => p.ProductLineID).IsUnique();
 
-
 				//relation configurations
 				builder.HasMany(p => p.OrderProducts)
                     .WithOne(o => o.Product)
                     .HasForeignKey(o => o.ProductId)
                     .OnDelete(DeleteBehavior.Cascade);
+
                 builder.HasOne(p => p.ProductLine)
                     .WithMany(pr => pr.Products)
                     .HasForeignKey(p => p.ProductLineID);
@@ -50,11 +49,12 @@ namespace SalesSystem.Configuration
 
 				//property configurations
                 builder.HasKey(o => o.Id);
-                builder.Property(o => o.CustomerId).HasColumnType("int").HasPrecision(10,0);
-                builder.Property(o => o.Orderdate).IsRequired();
-                builder.Property(o => o.Shippeddate).IsRequired(false);
+				builder.Property(p => p.Id).HasColumnType("int").IsRequired();
+
+				builder.Property(o => o.Orderdate).IsRequired();
+                builder.Property(o => o.Shippeddate).IsRequired();
                 builder.Property(o => o.Requireddate).IsRequired();
-                builder.Property(o => o.Status).IsRequired(false);
+                builder.Property(o => o.Status).IsRequired();
                 builder.Property(o => o.Comments).HasMaxLength(500).IsRequired(false);
 
 
@@ -73,6 +73,7 @@ namespace SalesSystem.Configuration
                     .WithOne(or => or.Order)
                     .HasForeignKey(o => o.OrderId)
                     .OnDelete(DeleteBehavior.Cascade);
+
                 builder.HasOne(o => o.Customer)
                     .WithMany(c => c.Orders)
                     .HasForeignKey(o => o.CustomerId)
@@ -87,9 +88,9 @@ namespace SalesSystem.Configuration
 
                 //property configurations
                 builder.HasKey(c => c.Id);
-                builder.Property(c => c.Id).HasColumnType("int").HasPrecision(10, 0);
+                //builder.Property(c => c.Id).HasColumnType("int").HasPrecision(10, 0);
+                //builder.Property(c => c.SalesRepEmployeNumber).IsRequired(false);
 				builder.Ignore(c => c.Name);
-                builder.Property(c => c.SalesRepEmployeNumber).HasColumnType("int").HasPrecision(10, 0).IsRequired();
                 builder.Property(c => c.FName).HasMaxLength(50).IsRequired(false).HasColumnName("FirstName");
                 builder.Property(c => c.LName).HasMaxLength(50).IsRequired(false).HasColumnName("LastName");
                 builder.Property(c => c.Address1).HasMaxLength(100).IsRequired(false).HasColumnName("address");
@@ -99,29 +100,28 @@ namespace SalesSystem.Configuration
                 builder.Property(c => c.Country).HasMaxLength(50).IsRequired(false);
                 builder.Property(c => c.Phone).HasMaxLength(20).IsRequired(false);
                 builder.Property(c => c.State).HasMaxLength(50).IsRequired(false);
-                builder.Property(c => c.CreditLimit).HasColumnType("decimal(19,0)").IsRequired(false);
+                builder.Property(c => c.CreditLimit).HasColumnType("decimal(19,0)").IsRequired();
 
 				//constraints
                 builder.HasIndex(c => new { c.Address1, c.Address2, c.City, c.Region, c.Country,c.FName,c.LName }).IsUnique();//not allow cust same name and same address 
-
 				//property configurations
 
-                //no need to make it again
+				//no need to make it again
 				//builder.HasMany(c => c.Orders)
-    //                .WithOne(o => o.Customer)
-    //                .HasForeignKey(c => c.CustomerId)
-    //                .OnDelete(DeleteBehavior.Cascade);
+				//                .WithOne(o => o.Customer)
+				//                .HasForeignKey(c => c.CustomerId)
+				//                .OnDelete(DeleteBehavior.Cascade);
 
-                builder.HasMany(c => c.Payments)
+				builder.HasMany(c => c.Payments)
                     .WithOne(p => p.Customer)
                     .HasForeignKey(p => p.CustomerId)
-                    .OnDelete(DeleteBehavior.SetNull);
+                    .OnDelete(DeleteBehavior.Cascade);
 
-                builder.HasOne(c => c.Employee)
-                    .WithMany(e => e.Customers)
-                    .HasForeignKey(c => c.SalesRepEmployeNumber)
-                    .OnDelete(DeleteBehavior.SetNull); 
-            }
+                builder.HasMany(c => c.Employeecustomers)
+                    .WithOne(ec => ec.Customer)
+                    .HasForeignKey(ec => ec.Custid)
+                    .OnDelete(DeleteBehavior.Cascade);
+			}
         }
 
         public class EmployeeConfiguration:IEntityTypeConfiguration<Employee>
@@ -130,34 +130,38 @@ namespace SalesSystem.Configuration
             {
 				//property configurations
 				builder.HasKey(e => e.Id);
-                builder.Property(e => e.Id).HasColumnType("int").HasPrecision(10, 0).IsRequired();
-				builder.Property(e => e.Managerid).HasColumnType("int").HasPrecision(10, 0).IsRequired(false);
-				builder.Property(e => e.OfficeCode).IsRequired();
 				builder.Property(e => e.FName).HasMaxLength(255).IsRequired(false).HasColumnName("FirstName");
 				builder.Property(e=>e.LName).HasMaxLength(255).IsRequired(false) .HasColumnName("LastName");
                 builder.Property(e => e.Email).HasMaxLength(255).IsRequired(false);
 				builder.Property(e => e.Extension).HasMaxLength(255).IsRequired(false);
-                builder.Property(e => e.JobTitle).HasMaxLength(100).IsRequired();
+				builder.Property(e => e.Managerid).IsRequired(false);
+
+				builder.Property(e => e.JobTitle).HasMaxLength(100).IsRequired();
+
+
 
 				//constraints
                 builder.HasIndex(e=>e.Email).IsUnique();
+
+
 
 				//relationship configurations
 				builder.HasOne(e => e.Manager)
                     .WithMany(m => m.Employees)
                     .HasForeignKey(e => e.Managerid)
-                    .OnDelete(DeleteBehavior.SetNull);
-                //no need to make it again
-				//builder.HasMany(e => e.Customers)
-    //                .WithOne(c => c.Employee)
-    //                .HasForeignKey(c => c.SalesRepEmployeNumber)
-    //                .OnDelete(DeleteBehavior.SetNull);
+                    .OnDelete(DeleteBehavior.NoAction);
 
                 builder.HasOne(e => e.Office)
                     .WithMany(o => o.Employees)
                 .HasForeignKey(e => e.OfficeCode)
                 .OnDelete(DeleteBehavior.Cascade);
-            }
+
+
+                builder.HasMany(e => e.Employeecustomers)
+                    .WithOne(ec => ec.Employee)
+                    .HasForeignKey(ec => ec.Empid)
+                    .OnDelete(DeleteBehavior.Cascade);
+			}
         }
 
         public class OfficeConfiguration:IEntityTypeConfiguration<Office>
@@ -166,19 +170,21 @@ namespace SalesSystem.Configuration
             {
 				//property configurations
 				builder.HasKey(o=>o.ID);
-                builder.Property(o => o.ID).HasColumnType("int").HasPrecision(10, 0);
+                //builder.Property(o => o.ID).HasColumnType("int").HasPrecision(10, 0);
                 builder.Property(o => o.City).HasMaxLength(255).IsRequired(false);
                 builder.Property(o => o.Phone).HasMaxLength(255).IsRequired(false);
                 builder.Property(o => o.ADDress1).HasMaxLength(255).IsRequired(false).HasColumnName("address");
                 builder.Property(o => o.ADDress2).HasMaxLength(255).IsRequired(false).HasColumnName("second address");
                 builder.Property(o => o.State).HasMaxLength(255).IsRequired(false);
                 builder.Property(o => o.Country).HasMaxLength(255).IsRequired(false);
-                builder.Property(o => o.PostalCode).IsRequired(false);
+                builder.Property(o => o.PostalCode).IsRequired();
                 builder.Property(o => o.Territory).HasMaxLength(255).IsRequired(false);
 
 
 				//constraints
 				builder.HasIndex(o => new { o.State,o.Country,o.City,o.ADDress1, o.ADDress2 }).IsUnique();
+
+                //builder.HasCheckConstraint("")
 
 				//relationship configurations no need to make it again
 				//builder.HasMany(o=>o.Employees)
@@ -195,10 +201,11 @@ namespace SalesSystem.Configuration
 
                 //property configuration
                 builder.HasKey(p => p.Checknum);
+                builder.Property(p => p.CustomerId).IsRequired();
                 builder.Property(p => p.Checknum).HasMaxLength(255);
-				builder.Property(p => p.PaymebntDate).IsRequired(false);
+				builder.Property(p => p.PaymebntDate).IsRequired();
 				builder.Property(p => p.CustomerId).HasColumnType("int").HasPrecision(10,0).IsRequired();
-                builder.Property(p => p.Amount).HasColumnType("decimal(19,0)").IsRequired(false);
+                builder.Property(p => p.Amount).HasColumnType("decimal(19,0)").IsRequired();
 
                 //make it before no need to make it again
 				//builder.HasOne(p => p.Customer)
@@ -216,7 +223,7 @@ namespace SalesSystem.Configuration
 
                 //property configuration
                 builder.HasKey(p => p.ID);
-                builder.Property(p => p.ID).HasColumnType("int").HasPrecision(10, 0);
+                //builder.Property(p => p.ID).HasColumnType("int").HasPrecision(10, 0);
                 builder.Property(p => p.DescLNText).HasMaxLength(255).IsRequired(false);
 				builder.Property(p => p.DescLNHtml).HasMaxLength(255).IsRequired(false);
 				builder.Property(p => p.Image).HasMaxLength(100).IsRequired(false);
@@ -235,11 +242,10 @@ namespace SalesSystem.Configuration
             {
                 //property configuration
                 builder.HasKey(o => o.ID);
-                builder.Property(o => o.ID).HasColumnType("int").HasPrecision(10, 0);
-				builder.Property(o => o.OrderId).HasColumnType("int").HasPrecision(10, 0).IsRequired();
-				builder.Property(o => o.ProductCode).HasColumnType("int").HasPrecision(10, 0).IsRequired();
-				builder.Property(o => o.Quantity).HasColumnType("int").HasPrecision(10, 0).IsRequired(false);
-				builder.Property(o => o.ProductCode).HasColumnType("decimal(19,0)").IsRequired(false);
+                //builder.Property(o => o.ID).HasColumnType("int").HasPrecision(10, 0);
+				builder.Property(o => o.OrderId).HasColumnType("int").IsRequired();
+				builder.Property(o => o.ProductId).HasColumnType("int").IsRequired();
+				builder.Property(o => o.Quantity).HasColumnType("int").IsRequired();
 
                 // make it no need to make it again
 				//builder.HasOne(or => or.Product)
